9776d352b5f76a28583d4ff9f10f6c63e81297b8
abubjazov <abubjazov@proton.me>
2026-08-15 23:04:13 +0300

    added lesson 1

---
+++ miles_to_km.rb
@@ -0,0 +1,6 @@
+puts "Введите значение в милях!"
+
+user_input_in_miles = gets.to_f
+result_in_kilometers = user_input_in_miles * 1.4
+
+puts "#{user_input_in_miles} миль это #{result_in_kilometers} километров."

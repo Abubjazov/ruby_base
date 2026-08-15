440eed4ffafea7715dcdb5d82e8afc2b6b6646e2
abubjazov <abubjazov@proton.me>
2026-08-16 01:01:31 +0300

    added guesser.rb

---
+++ guesser.rb
@@ -0,0 +1,13 @@
+require_relative 'terminal_logger'
+
+RIGHT_GUESS = TerminalLogger.render_success("УГАДАЛ!!! )")
+WRONG_GUESS = TerminalLogger.render_error("НЕ УГАДАЛ (")
+
+random_guess = (rand * 10).to_i + 1
+
+puts "Я загОдал число от 1 до 10! ОтгОдай!!! )"
+puts random_guess
+
+guess = gets.to_i
+
+puts random_guess == guess ? RIGHT_GUESS : WRONG_GUESS

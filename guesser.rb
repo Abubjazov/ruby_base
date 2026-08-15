a6f70de2a4175904cbc3e097b6ba741a7b2fb6a5
abubjazov <abubjazov@proton.me>
2026-08-16 01:31:17 +0300

    added game cycle to guesser.rb

--- guesser.rb
+++ guesser.rb
@@ -1,13 +1,30 @@
 require_relative 'terminal_logger'
 
-RIGHT_GUESS = TerminalLogger.render_success("УГАДАЛ!!! ＼(★^∀^★)／")
-WRONG_GUESS = TerminalLogger.render_error("НЕ УГАДАЛ!!! (╯°□°)╯︵ ┻━┻")
+RIGHT_GUESS = TerminalLogger.render_success("ТЫ УГАДАЛ!!! ＼(★^∀^★)／")
+WRONG_GUESS = TerminalLogger.render_error("Попытки кончились, ты проиграл!  (╯°□°)╯︵ ┻━┻")
 
 random_guess = (rand * 10).to_i + 1
+has_won = false
 
 puts "Я загОдал число от 1 до 10! ОтгОдай!!! )"
 puts random_guess
 
-guess = gets.to_i
+3.times do |i|
+  puts "Попытка #{i + 1}/3. Введи число: "
+  guess = gets.to_i
 
-puts random_guess == guess ? RIGHT_GUESS : WRONG_GUESS
+  if random_guess == guess 
+    puts RIGHT_GUESS
+    has_won = true
+    break 
+  elsif guess < random_guess 
+    puts "Моё число бОООльше... (‾◡◝)"
+  else
+    puts "Моё число мЕЕЕньше... ┐(‘～;)┌"
+  end
+end
+
+unless has_won
+  puts "Правильное число было: #{random_guess}"
+  puts WRONG_GUESS
+end

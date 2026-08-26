c6e9ec0184d7dc9b0ee7d6ebbecb50fc0904b74c
abubjazov <abubjazov@proton.me>
2026-08-26 18:14:05 +0300

    feat: begin development of DND game
    - Added the basic directory structure for dnd_game.
    - Set up initial files to start implementation.

---
+++ dnd_game/game_engine.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+# Класс для движка игры
+class Engine
+  # attr_reader :correct_answers_count, :incorrect_answers_count
+
+  # def initialize
+
+  # end
+
+  def run; end
+
+  # private
+
+  # def user_name
+
+  # end
+end

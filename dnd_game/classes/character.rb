b9efd15111772039571954f58acce919224bfa78
abubjazov <abubjazov@proton.me>
2026-08-26 22:27:45 +0300

    feat: add Character class

---
+++ dnd_game/classes/character.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Класс для движка игры
+class Character
+  attr_reader :max_hit_points, :hit_points, :character_type
+
+  def initialize(min_dmg:, max_dmg:, hit_points:, character_type:)
+    @max_hit_points = hit_points.freeze
+    @hit_points = hit_points
+    @min_dmg = min_dmg
+    @max_dmg = max_dmg
+    @character_type = character_type
+
+    post_initialize
+  end
+
+  def post_initialize; end
+
+  def attack(target)
+    dmg = rand @min_dmg..@max_dmg
+    target.receive_dmg dmg
+
+    dmg
+  end
+
+  def receive_dmg(dmg)
+    @hit_points -= dmg
+  end
+end
